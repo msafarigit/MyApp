89d67e14938b49fa2b7c3977fd3162655c56cffd
msafarigit <msafari.int@gmail.com>
2018-12-14 14:23:30 +0330

    edit setting content

--- MyApp/MyApp/Pages/SettingsPage.xaml.cs
+++ MyApp/MyApp/Pages/SettingsPage.xaml.cs
@@ -1,20 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MyApp.Pages
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class SettingsPage : ContentPage
-	{
-		public SettingsPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            InitializeSettings();
+            base.OnAppearing();
+        }
+
+        private void InitializeSettings()
+        {
+            //displayName.Text = "Mahdi";
+            //bioEditor.Text = "Mahdi is developing Xamarin forms for interesting!!!";
+            //articlesCountSlider.Value = 5;
+            //categoryPicker.SelectedIndex = 2;
+        }
+    }
 }
